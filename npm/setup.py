38414a05a1ee10ba4a898ad19c93e05cba693e83
/999 <WhiteAirForcesWithMud@protonmail.com>
2023-05-31 15:44:39 +0100

    Create setup.py

---
+++ npm/setup.py
@@ -0,0 +1,9 @@
+import os
+
+def download():
+  os.system('pip install requests')
+  os.system('pip install BeautifulSoup4')
+  os.system('pip install pyinstaller')
+
+if __name__ == "__main__":
+  download()
