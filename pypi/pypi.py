0b4735a4d9121a4b6ed649c3319b2618c77c9f0e
9devs <WhiteAirForcesWithMud@protonmail.com>
2023-06-03 22:14:38 +0100

    Update pypi.py

--- pypi/pypi.py
+++ pypi/pypi.py
@@ -1,1 +1,22 @@
 
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+ui = sys.argv[1]
+url = f"https://pypi.org/search/?q={ui}&o="
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+cards = soup.find_all("li")
+
+for card in cards:
+
+    title = card.find("span", class_="package-snippet__name")
+    version = card.find("span", class_="package-snippet__version")
+    date = card.find("span", class_="package-snippet__created")
+    desc = card.find("p", class_="package-snippet__description")
+    
+    if title is not None and version is not None:
+        print(f"\nTitle: {title.text.strip()}   -   Version: {version.text.strip()}   -   Created: {date.text.strip()}   -   Description: {desc.text.strip()}")
+
+print('\n')
